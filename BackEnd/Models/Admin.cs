2ce0a47674edf97142e9206494ffea12445a46f7
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-02-18 16:54:57 -0500

    Setup Test

---
+++ BackEnd/Models/Admin.cs
@@ -0,0 +1,8 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models;
+public class Admin : User
+{
+
+}
--- BackEnd/Models/User.cs
+++ BackEnd/Models/User.cs
@@ -7,5 +7,6 @@ public class User
 {
     [Key]
     public int UserId {get;set;}
+    [Required]
     public string UserName {get;set;}
 }
