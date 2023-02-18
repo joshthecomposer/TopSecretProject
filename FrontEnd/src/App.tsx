e1335bb40792db03f3ee6300df1066926c20bf2a
Robert Teets <teetsrobert@gmail.com>
2023-02-18 16:42:08 -0700

    feat: Basic testing with fetch api and c# server.

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,5 +1,38 @@
-function App() {
-    return <div></div>;
+import { useEffect, useState } from "react";
+
+interface User {
+    userId: number;
+    userName: string;
 }
+
+function App() {
+    const [testData, setTestData] = useState<User>();
+
+    useEffect(() => {
+        fetchData()
+            .then((result) => {
+                setTestData(result);
+                console.log(result);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div>
+            {testData?.userId}: {testData?.userName}
+        </div>
+    );
+}
+
+const fetchData = async () => {
+    const data = await fetch("http://localhost:5014/user");
+
+    if (!data.ok) return null;
+
+    const json = await data.json();
+    return json;
+};
 
 export default App;
