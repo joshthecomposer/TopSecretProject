000e84fa77b0246e7b41cf315a09f2599a918aaa
melissa-24 <melissalongenberger24@gmail.com>
2023-02-19 18:14:34 -0500

    updated create user route to match front end

--- BackEnd/Controllers/MainController.cs
+++ BackEnd/Controllers/MainController.cs
@@ -25,7 +25,7 @@ public class MainController : ControllerBase
         return user;
     }
 
-    [HttpPost("/api/employee/create")]
+    [HttpPost("/api/admin/create")]
     public async Task<ActionResult<User>> CreateEmployee([FromBody]User emp)
     {
         if (ModelState.IsValid)
