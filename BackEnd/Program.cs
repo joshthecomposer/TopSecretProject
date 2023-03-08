e40dc163c5a407959f5d7bc87a6ad7bae5f20804
Robert Teets <teetsrobert@gmail.com>
2023-03-08 15:01:21 -0700

    chore: fixed cors on backend.

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.AllowAnyOrigin().AllowAnyHeader();
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
         }
     );
 });
