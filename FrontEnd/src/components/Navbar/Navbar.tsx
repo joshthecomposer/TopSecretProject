4e4a25d27479a5e5a58493e67a49520aa56ff2af
Robert Teets <teetsrobert@gmail.com>
2023-03-08 09:20:05 -0700

    refactor: broke navbar out into seperate component.

---
+++ FrontEnd/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { BsFillHouseDoorFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import cookies from "../../lib/stores";
+import { RiLogoutBoxFill } from "react-icons/ri";
+
+export default function Navbar() {
+    const [expandNav, setExpandNav] = useState(false);
+    const navigate = useNavigate();
+
+    return (
+        <div
+            onMouseEnter={() => setExpandNav(true)}
+            onMouseLeave={() => setExpandNav(false)}
+            className="h-[100svh] p-4 flex flex-col justify-center items-center bg-[#2a2a40] w-fit fixed"
+        >
+            <a
+                href="#"
+                className="flex items-center justify-start relative transition-all duration-500 p-3 rounded-full shadow-lg bg-[#32324d] hover:bg-[#ADADF6] text-[#ADADF6] hover:text-[#2a2a40]"
+                style={{
+                    width: expandNav ? 256 : 56,
+                }}
+            >
+                <BsFillHouseDoorFill size={32} className="flex-none" />
+                <p
+                    className="absolute right-3 font-bold transition-opacity"
+                    style={{
+                        opacity: expandNav ? 100 : 0,
+                        transitionDuration: expandNav ? "500ms" : "400ms",
+                    }}
+                >
+                    Home
+                </p>
+            </a>
+            <hr className="h-[3px] my-4 border-0 bg-[#ADADF6] w-full" />
+            <hr className="h-[3px] my-4 border-0 bg-[#ADADF6] w-full" />
+            <button
+                onClick={() => {
+                    cookies.useLogout();
+                    navigate("/login");
+                }}
+                className="flex items-center justify-start relative transition-all duration-500 p-3 rounded-full shadow-lg bg-[#32324d] hover:bg-[#ADADF6] text-[#ADADF6] hover:text-[#2a2a40]"
+                style={{
+                    width: expandNav ? 256 : 56,
+                }}
+            >
+                <RiLogoutBoxFill size={32} className="flex-none" />
+                <p
+                    className="absolute right-3 font-bold transition-opacity"
+                    style={{
+                        opacity: expandNav ? 100 : 0,
+                        transitionDuration: expandNav ? "500ms" : "400ms",
+                    }}
+                >
+                    Logout
+                </p>
+            </button>
+        </div>
+    );
+}
--- FrontEnd/src/pages/Dashboard.tsx
+++ FrontEnd/src/pages/Dashboard.tsx
@@ -1,9 +1,7 @@
 import cookies from "../lib/stores";
 import { useState } from "react";
-import { useNavigate, Navigate } from "react-router-dom";
-import { DiAptana } from "react-icons/di";
-import { BsFillHouseDoorFill } from "react-icons/bs";
-import { RiLogoutBoxFill } from "react-icons/ri";
+import { Navigate } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 export interface IUserToken {
     employeeId: number;
@@ -12,63 +10,17 @@ export interface IUserToken {
 }
 
 export default function () {
-    const navigate = useNavigate();
     const [userToken, setUserToken] = useState<IUserToken | null>(
         cookies.cookies.get("userCookie")
     );
-    const [expandNav, setExpandNav] = useState(false);
 
     if (!userToken) {
         return <Navigate to="/login" />;
     }
 
     return (
-        <div
-            onMouseEnter={() => setExpandNav(true)}
-            onMouseLeave={() => setExpandNav(false)}
-            className="h-[100svh] p-4 flex flex-col justify-center items-center bg-[#2a2a40] w-fit"
-        >
-            <a
-                href="#"
-                className="flex items-center justify-start relative transition-all duration-500 p-3 rounded-full shadow-lg bg-[#32324d] hover:bg-[#ADADF6] text-[#ADADF6] hover:text-[#2a2a40]"
-                style={{
-                    width: expandNav ? 256 : 56,
-                }}
-            >
-                <BsFillHouseDoorFill size={32} className="flex-none" />
-                <p
-                    className="absolute right-3 font-bold transition-opacity"
-                    style={{
-                        opacity: expandNav ? 100 : 0,
-                        transitionDuration: expandNav ? "500ms" : "400ms",
-                    }}
-                >
-                    Home
-                </p>
-            </a>
-            <hr className="h-[3px] my-4 border-0 bg-[#ADADF6] w-full" />
-            <hr className="h-[3px] my-4 border-0 bg-[#ADADF6] w-full" />
-            <button
-                onClick={() => {
-                    cookies.useLogout();
-                    navigate("/login");
-                }}
-                className="flex items-center justify-start relative transition-all duration-500 p-3 rounded-full shadow-lg bg-[#32324d] hover:bg-[#ADADF6] text-[#ADADF6] hover:text-[#2a2a40]"
-                style={{
-                    width: expandNav ? 256 : 56,
-                }}
-            >
-                <RiLogoutBoxFill size={32} className="flex-none" />
-                <p
-                    className="absolute right-3 font-bold transition-opacity"
-                    style={{
-                        opacity: expandNav ? 100 : 0,
-                        transitionDuration: expandNav ? "500ms" : "400ms",
-                    }}
-                >
-                    Logout
-                </p>
-            </button>
-        </div>
+        <>
+            <Navbar />
+        </>
     );
 }
